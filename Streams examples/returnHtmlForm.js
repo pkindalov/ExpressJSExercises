15607de92ff2f6453923ae69e1e0d5decfbd8805
pkindalov <panayotkindalov@gmail.com>
2017-05-26 00:45:27 +0300

    Added returnHtmlForm.js

---
+++ Streams examples/returnHtmlForm.js
@@ -0,0 +1,39 @@
+const http = require('http');
+const query = require('querystring');
+const port = 1337;
+
+//How to return a regular html form with readable stream. NOTE: THIS NOT WORK WITH FORM WITH IMAGES
+
+http
+     .createServer((req, res) => {
+        if(req.method === 'GET'){
+
+            res.writeHead(200, {
+                'Content-Type':  'text/html'
+            });
+
+            res.write(`
+                <form method="POST">
+                    <input type="text" name="username" /><br />
+                    <input type="password" name="password" /><br />
+                    <input type="submit" value="Send" />
+                </form>
+            `);
+
+            res.end();
+        } else if(req.method === "POST"){
+            let result = '  ';
+            req.on('data', data => {result += data;});
+            req.on('end', () => {
+                let parsedResult = query.parse(result);
+                console.log(parsedResult.username);
+                console.log(parsedResult.password);
+            });
+
+            res.write('Thank you :) ');
+            res.end();
+        }
+     })
+     .listen(port);
+
+     console.log("Server listening on port " + port);
