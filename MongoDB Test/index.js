dd37007b2802a28868992bde886d8abd71be408b
pkindalov <panayotkindalov@gmail.com>
2017-05-27 01:40:45 +0300

    Updare index.js

--- MongoDB Test/index.js
+++ MongoDB Test/index.js
@@ -1,3 +1,29 @@
 const mongodb = require('mongodb');
 
-// mongodb.MongoClient
+let connection = 'mongodb://localhost:27017/mycustomexpressdb';
+
+mongodb.MongoClient.connect(connection, (err, db) => {
+    if(err){
+        console.log(err);
+        return;
+    }
+
+    let cats = db.collection('cats');
+
+    cats
+        .insertMany([
+            {'name': 'Vankata', 'age':15},
+            {'name': 'Pesho', 'age':5},
+            {'name': 'Plamen', 'age':1},
+            {'name': 'Dichoni', 'age':25, 'color': 'yellow'}
+        ], (err, result) => {
+            if(err){
+                console.log(err);
+                return;
+            }
+
+            console.log(result);
+        });
+
+
+});
